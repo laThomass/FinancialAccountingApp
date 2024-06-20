8c890f66c96f9a2e1dcba6687f0a53295d0b6f4a
laThomass <lathomasinc@gmail.com>
2024-06-20 12:58:36 -0400

    Edited so it can run on terminal

--- src/main/AlphaVantageDemo.java
+++ src/main/AlphaVantageDemo.java
@@ -14,9 +14,9 @@ import javax.swing.*;
 public class AlphaVantageDemo {
 
   public static void main(String[] args) {
-    String interfaceType = "GUI"; // Change this to "Text" to use the text-based interface
+    String interfaceType = args.length > 0 ? args[0] : "GUI";
 
-    if (interfaceType.equalsIgnoreCase("Text")) {
+    if (interfaceType.equalsIgnoreCase("-Text")) {
       StockView view = new StockView();
       AlphaAPI api = new AlphaAPI();
       StockController controller = new StockController(view,
@@ -31,7 +31,7 @@ public class AlphaVantageDemo {
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
-    } else if (interfaceType.equalsIgnoreCase("GUI")) {
+    } else if (interfaceType.equalsIgnoreCase("-GUI")) {
       SwingUtilities.invokeLater(() -> {
         AlphaAPI api = new AlphaAPI();
         IGUIView guiView = new StockViewGUI();
@@ -39,10 +39,9 @@ public class AlphaVantageDemo {
                 new InputStreamReader(System.in), System.out, api);
         guiView.setController(controller);
 
-        // Create and show the GUI
         JFrame frame = new JFrame("Stock Management System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(guiView.getMainPanel()); // Assuming getMainPanel() returns the main JPanel of the GUI
+        frame.setContentPane(guiView.getMainPanel());
         frame.pack();
         frame.setVisible(true);
 
@@ -55,7 +54,7 @@ public class AlphaVantageDemo {
         }
       });
     } else {
-      System.out.println("Invalid interface type. Please choose 'Text' or 'GUI'.");
+      System.out.println("Invalid interface type. Please choose '-Text' or '-GUI'.");
     }
   }
 }
